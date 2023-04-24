e4521e6a026aff2dfd19f5e23b1ecfce858abec2
Nivaldo Melo <nivaldomelofaijane@gmail.com>
2023-04-23 22:12:29 -0300

    Atualizacao

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended",
+    "prettier/@typescript-eslint",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["prettier", "@typescript-eslint"],
+  rules: {},
+};
